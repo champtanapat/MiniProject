1db2aa0f2510f31a8dfb5faab8ebd6c2431731a8
Tanapat Permpoon <champ_vs_the.dancefloor@hotmail.com>
2019-05-25 01:08:26 +0700

    no message

--- ABCOnline/ABCOnline/Models/Member.cs
+++ ABCOnline/ABCOnline/Models/Member.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,27 @@ namespace ABCOnline.Models
 {
     public class Member
     {
+        [Display(Name = "Fist Name")]
+        [Required(ErrorMessage = "กรุณากรอกข้อมูลให้ครบถ้วน")]
+        public string firstName { get; set; }
+
+
+        [Display(Name = "Last Name")]
+
+        [Required(ErrorMessage = "กรุณากรอกข้อมูลให้ครบถ้วน")]
+        public string lastName { get; set; }
+
+
+        [Required(ErrorMessage = "กรุณากรอกข้อมูลให้ครบถ้วน")]
+        public string address { get; set; }
+
+
+        [Required(ErrorMessage = "กรุณากรอกข้อมูลให้ครบถ้วน")]
+        public string age { get; set; }
+
+
+
+        [Required(ErrorMessage = "กรุณากรอกข้อมูลให้ครบถ้วน")]
+        public string birthday { get; set; }
     }
 }
