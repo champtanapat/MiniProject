f5fb614caf29a920981915eba536e19bc755a686
Tanapat Permpoon <champ_vs_the.dancefloor@hotmail.com>
2019-05-26 17:38:39 +0700

    Revert "updaet Final"
    This reverts commit 551233bb95218261d59af9067c703479cb3ecd00.

--- ABCOnline/ABCOnline/Controllers/RegisterController.cs
+++ ABCOnline/ABCOnline/Controllers/RegisterController.cs
@@ -1,9 +1,6 @@
 ﻿using ABCOnline.Models;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,9 +11,6 @@ namespace ABCOnline.Controllers
     {
         // GET: Register
         TestEntities1 db = new TestEntities1();
-        SqlCommand cmd = null;
-        SqlConnection sqlConn = null;
-        SqlDataReader reader = null;
         public ActionResult Index(int? Id)
         {
             Registers register = new Registers();
@@ -56,9 +50,9 @@ namespace ABCOnline.Controllers
             {
 
             }
-            var allRegister = (from s in db.Registers select s).ToList();
-            ViewBag.dataList = allRegister;
-            return PartialView("_AddMember");
+            //var allRegister = (from s in db.Registers select s).ToList();
+            //ViewBag.dataList = allRegister;
+            return RedirectToAction("Index");
         }
 
         private int CalAge(DateTime? birthday)
@@ -96,13 +90,14 @@ namespace ABCOnline.Controllers
                         };
               
                 }
+            var allRegister = (from s in db.Registers select s).ToList();
+            ViewBag.dataList = allRegister;
             return View("Index", register);
         }
 
-        public ActionResult Delete(int? Id) 
+        public ActionResult Delete(int? Id)
         {
-            SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["connectBase"].ConnectionString);
-            cnn.Open();
+           
             var del = db.Registers.Find(Id);
             try
             {
@@ -112,30 +107,14 @@ namespace ABCOnline.Controllers
                 }
                 else
                 {
-                    string sql = "EXEC  dbo.DeleteMember @IdMember ";
-                    cmd = new SqlCommand(sql);
-                    cmd.Connection = cnn;
-                    cmd.Parameters.Add("@IdMember", SqlDbType.NVarChar, 10);
-                    cmd.Parameters["@IdMember"].Value = Id.ToString();
-                    var ss = cmd.ExecuteNonQuery();
-                    cnn.Close();
-
-                    //reader = cmd.ExecuteReader();
-                    //Register register = new Register();
-                    //while (reader.Read())
-                    //{
-                    //    register.FirstName = reader["FirstName"].ToString();
-                    //    register.LastName = reader["LastName"].ToString();
-                    //    register.Address = reader["Address"].ToString();
-                    //    register.Age = reader["Age"] != null ? Convert.ToInt32(reader["Age"]) : 0;
-                    //}
-
+                    db.Registers.Remove(del);
+                    db.SaveChanges();
                 }
             }
             catch (Exception ex)
             {
+               
             }
-
             return RedirectToAction("Index");
         }
 
